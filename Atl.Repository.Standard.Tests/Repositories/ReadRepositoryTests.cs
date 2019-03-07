46f9712397c16373657e3271eeb28fa264ae69b8
Mahmudul Islam <mahmud6120@gmail.com>
2019-03-07 22:24:18 +1100

    Refactored Unit Tests

--- Atl.Repository.Standard.Tests/Repositories/ReadRepositoryTests.cs
+++ Atl.Repository.Standard.Tests/Repositories/ReadRepositoryTests.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Atl.Repository.Standard.Repositories.Contracts;
+using Atl.Repository.Standard.Repositories.Implementations;
 using Atl.Repository.Standard.Tests.Fixtures;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Atl.Repository.Standard.Tests.Repositories
 {
-	public class ReadRepositoryTests : IClassFixture<TestRepository>
+	public class ReadRepositoryTests
 	{
 		private readonly IGenericRepository<int> _repo;
 		private readonly TestRepository _testRepo;
 
-		public ReadRepositoryTests(TestRepository testRepo)
+		public ReadRepositoryTests()
 		{
-			_repo = testRepo.Repository;
-			_testRepo = testRepo;
+            _testRepo = new TestRepository();
+			_repo = _testRepo.Repository;
 		}
 
 		[Fact]
-		public void TestAll()
+		public void GetAll_Should_Return_Successfully()
 		{
 			//save entity
 			var tenant = new TestDatabaseContext.Tenant()
@@ -31,7 +33,6 @@ namespace Atl.Repository.Standard.Tests.Repositories
 			_testRepo.TContext.ContextId = tenant.Id;
 			var org = new TestDatabaseContext.Organization()
 			{
-				
 			};
 			org = _repo.Add(org);
 			_testRepo.OrgContext.ContextId = org.Id;
@@ -42,22 +43,69 @@ namespace Atl.Repository.Standard.Tests.Repositories
 			_repo.Add(user);
 
 			//get
-			var userFethed = _repo.GetAll<TestDatabaseContext.User>().First();
-			Assert.Equal(userFethed.Id, user.Id);
-			Assert.Equal(10, userFethed.Id);
+			var userFetched = _repo.GetAll<TestDatabaseContext.User>()
+                .First();
+			Assert.Equal(userFetched.Id, user.Id);
+			Assert.Equal(10, userFetched.Id);
 
 			//update
-			userFethed.IsLocked = true;
-			_repo.Update(userFethed);
+			userFetched.IsLocked = true;
+			_repo.Update(userFetched);
 
-			userFethed = _repo.GetAll<TestDatabaseContext.User>().First();
-			Assert.True(userFethed.IsLocked);
+			userFetched = _repo.GetAll<TestDatabaseContext.User>().First();
+			Assert.True(userFetched.IsLocked);
 
 			//delete
 			_repo.Delete(user);
 
-			userFethed = _repo.GetAll<TestDatabaseContext.User>().FirstOrDefault();
-			Assert.Null(userFethed);
+			userFetched = _repo.GetAll<TestDatabaseContext.User>().FirstOrDefault();
+			Assert.Null(userFetched);
 		}
-	}
+
+        [Fact]
+        public void GetAll_WithInclude_Should_Return_RelatedObject()
+        {
+            //save entity
+            var tenant = new TestDatabaseContext.Tenant()
+            {
+
+            };
+            tenant = _repo.Add(tenant);
+            _testRepo.TContext.ContextId = tenant.Id;
+            var org = new TestDatabaseContext.Organization()
+            {
+
+            };
+            org = _repo.Add(org);
+            _testRepo.OrgContext.ContextId = org.Id;
+            var user = new TestDatabaseContext.User()
+            {
+
+            };
+            _repo.Add(user);
+
+            //get
+            var userFetched = _repo.GetAll<TestDatabaseContext.User>()
+                .Include(x => x.Organization)
+                .Include(x => x.Tenant)
+                .First();
+            Assert.Equal(userFetched.Id, user.Id);
+            Assert.Equal(10, userFetched.Id);
+            Assert.NotNull(userFetched.Tenant);
+            Assert.NotNull(userFetched.Organization);
+
+            //update
+            userFetched.IsLocked = true;
+            _repo.Update(userFetched);
+
+            userFetched = _repo.GetAll<TestDatabaseContext.User>().First();
+            Assert.True(userFetched.IsLocked);
+
+            //delete
+            _repo.Delete(user);
+
+            userFetched = _repo.GetAll<TestDatabaseContext.User>().FirstOrDefault();
+            Assert.Null(userFetched);
+        }
+    }
 }
